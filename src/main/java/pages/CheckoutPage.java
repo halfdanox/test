6f4c5555992f3918c802882d38da38a8a670f908
DELL-E5570 <DELL-E5570@DESKTOP-CF1SHM6>
2017-08-07 14:01:17 +0300

    commit

--- src/main/java/pages/CheckoutPage.java
+++ src/main/java/pages/CheckoutPage.java
@@ -6,13 +6,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CheckoutPage extends Menu {
 	
-	public static final int WAIT_SECOND = 25;
+	public static final int WAIT_SECOND = 40;
 	 
 	@FindBy(id="addToCart")
 	WebElement addToCartButton;
