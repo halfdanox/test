85dea347cd1f9cfd37e5ffc9e628bb65280b15a1
Burak Tan <halfdanburak@gmail.com>
2017-08-07 13:56:06 +0300

    Update CheckoutPage.java

--- src/main/java/pages/CheckoutPage.java
+++ src/main/java/pages/CheckoutPage.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CheckoutPage extends Menu {
 	
-	public static final int WAIT_SECOND = 25;
+	public static final int WAIT_SECOND = 30;
 	 
 	@FindBy(id="addToCart")
 	WebElement addToCartButton;
